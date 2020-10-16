5f7f8442ee1ff37a51d4bd51a8372845b0f6671e
Comfort Ajala <comfort.ajala@ajalacomfort.com>
2020-10-17 00:41:24 +0200

    add cols

--- src/views/subviews/materiallist.tsx
+++ src/views/subviews/materiallist.tsx
@@ -1,11 +1,45 @@
 import React, { useEffect, useState } from "react"
 import {Table} from "antd"
 import { SmdashboardService } from "../../service";
-
-
+import moment from "moment";
+export interface IMaterial{
+    cost: number;
+description: string;
+material_type: string;
+number: number;
+purchase_date: Date;
+usage_date_in_month_year: Date
+_id: string
+}
+const cols : {
+    title: string;
+    dataIndex: string;
+    key: string;
+    render?: (...args: any) => any;
+  }[] = [{
+    title: "Purchase",
+    dataIndex: "purchase_date",
+    key: "purchase_date",
+    render:(purchase_date:string) => {
+        return moment(purchase_date).format("dddd Do MMM YYYY")
+      
+        
+    }
+  },{
+    title: "Type",
+    dataIndex: "material_type",
+    key: "material_type",
+    render:(material_type:string) => {
+        return material_type.toLowerCase()
+       }
+  },{
+    title: "Amount",
+    dataIndex: "number",
+    key: "number"
+  }]
 export const MaterialList: React.FC<any> = () => {
 
-        const [materials, setMaterials]: [any[], (...args:any) => any]  = useState([]);
+        const [materials, setMaterials]: [IMaterial[], (...args:any) => any]  = useState([]);
         useEffect(() => {
             SmdashboardService.fetchMaterials().then((ms) => {
                     setMaterials(ms)
@@ -14,7 +48,7 @@ export const MaterialList: React.FC<any> = () => {
         console.log(materials)
         return (
                 <div>
-                   sfdghjjgfd
+                   <Table dataSource={materials} columns={cols}/>
                 </div>
 
         )
