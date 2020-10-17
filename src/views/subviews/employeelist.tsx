defdabee662bb9416065e43142179e99a787133c
Comfort Ajala <comfort.ajala@ajalacomfort.com>
2020-10-17 21:45:33 +0200

    update

--- src/views/subviews/employeelist.tsx
+++ src/views/subviews/employeelist.tsx
@@ -3,14 +3,34 @@ import {Table} from "antd"
 import { SmdashboardService } from "../../service";
 import moment from "moment";
 export interface IEmployee{
-
+    address: string
+    date_joined: Date;
+    full_name: string;
+    income: number;
+    job_description: string;
+    job_title: string;
+    notes: string;
+    phone_number: string;
+    salary_type:string;
+    status: true;
+    __v: number;
+    _id:string;
 }
 const cols : {
     title: string;
     dataIndex: string;
     key: string;
     render?: (...args: any) => any;
-  }[] = []
+  }[] = [{
+    title: "Full name",
+    dataIndex: "full_name",
+    key: "full_name",
+  },
+  {
+    title: "Phone number",
+    dataIndex: "phone_number",
+    key: "phone_number"
+  }]
 export const EmployeeList: React.FC<any> = () => {
 
         const [employees, setEmployees]: [IEmployee[], (...args:any) => any]  = useState([]);
