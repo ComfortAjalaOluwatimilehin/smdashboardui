50b14d854bba21154611a539e28d54c435547f7b
Comfort Ajala <comfort.ajala@ajalacomfort.com>
2021-01-13 13:00:56 +0100

    update

--- src/views/customers/customers.tsx
+++ src/views/customers/customers.tsx
@@ -1,22 +1,19 @@
-import React from "react"
-import {Tabs } from "antd"
+import React from "react";
+import { Tabs } from "antd";
 import { CreateCustomer } from "./create";
-import { CustomerList, } from "./table";
+import { CustomerList } from "./table";
 
 const { TabPane } = Tabs;
 
-export const Customers : React.FC<any> = () => {
-
-
-        return (
-
-            <Tabs type="card">
-            <TabPane tab="Customers" key="1">
-             <CustomerList />
-            </TabPane>
-            <TabPane tab="New customer" key="2">
-              <CreateCustomer/>
-            </TabPane>
-          </Tabs>
-        )
-}
+export const Customers: React.FC<any> = () => {
+  return (
+    <Tabs type="card">
+      <TabPane tab="Customers" key="1">
+        <CustomerList />
+      </TabPane>
+      <TabPane tab="New customer" key="2">
+        <CreateCustomer />
+      </TabPane>
+    </Tabs>
+  );
+};
--- src/views/customers/table.tsx
+++ src/views/customers/table.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react"
-import {Table,} from "antd"
+import React, { useEffect, useState } from "react";
+import { Table } from "antd";
 import { SmdashboardService } from "../../service";
 import { ICreateCustomer } from "./create";
 
-
-const cols : {
-    title: string;
-    dataIndex: string;
-    key: string;
-    render?: (...args: any) => any;
-  }[] = [{
+const cols: {
+  title: string;
+  dataIndex: string;
+  key: string;
+  render?: (...args: any) => any;
+}[] = [
+  {
     title: "Full name",
     dataIndex: "fullName",
     key: "fullName",
@@ -17,22 +17,23 @@ const cols : {
   {
     title: "Phone number",
     dataIndex: "phoneNumber",
-    key: "phoneNumber"
-  }
-]
+    key: "phoneNumber",
+  },
+];
 export const CustomerList: React.FC<any> = () => {
+  const [customers, setCustomers]: [
+    ICreateCustomer[],
+    (...args: any) => any
+  ] = useState([]);
+  useEffect(() => {
+    SmdashboardService.fetchCustomers().then((response) => {
+      setCustomers(response);
+    });
+  }, []);
 
-        const [customers, setCustomers]: [ICreateCustomer[], (...args:any) => any]  = useState([]);
-        useEffect(() => {
-            SmdashboardService.fetchCustomers().then((response) => {
-                setCustomers(response)
-            })
-        }, [])
-
-        return (
-                <div>
-                   <Table dataSource={customers} columns={cols}/>
-                </div>
-
-        )
-}
+  return (
+    <div>
+      <Table dataSource={customers} columns={cols} />
+    </div>
+  );
+};
